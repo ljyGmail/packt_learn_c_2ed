e9312e3bf54e104f701d277e147b336883663ad9
liangjinyong <liangjy881@gmail.com>
2023-10-15 22:05:27 +0900

    ch01 running hello world

---
+++ ch01_running_hello_world/02_hello_comment.c
@@ -0,0 +1,16 @@
+/*
+ * 02_hello_comment.c
+ * My first C program with comments.
+ * by ljy
+ * created 2023/10/15
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    return 0;
+}
+
+/* eof */
